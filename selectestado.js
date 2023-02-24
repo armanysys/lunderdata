3928d60f4712c0c6766677c2af7dec1aa2b0be5c
Armando del Angel <113638993+aarmando2@users.noreply.github.com>
2023-02-24 16:12:11 -0600

    mapa

--- selectestado.js
+++ selectestado.js
@@ -443,6 +443,28 @@ estado_lista =  [
       "Montos": "NR"
     },
     {
+      "Entidad": "Baja California Sur",
+      "Denuncias": "NR",
+      "Investigaciones": "NR",
+      "Carpetas": "NR",
+      "Imputaciones": "NR",
+      "Vinculaciones": "NR",
+      "Sentencias": "NR",
+      "Judicialización": "NR",
+      "Criterio": "NR",
+      "Controversias": "NR",
+      "Suspensiónes": "NR",
+      "Procedimiento": "NR ",
+      "Reparaciónes": "NR",
+      "TotalCambate": "NR",
+      "Archivos": "NR",
+      "NoAcciónPenal": "NR",
+      "Abstenciones": "NR",
+      "TotalNoCombate": "NR",
+      "Presupuesto": "NR",
+      "Montos": "NR"
+    },
+    {
       "Entidad": "Puebla",
       "Denuncias": "1844",
       "Investigaciones": "51",
